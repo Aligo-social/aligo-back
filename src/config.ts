0790401206f19fe60e3212787cbc4dfa7c24837a
Pierre Marquet <marquet_pierre@yahoo.fr>
2025-02-20 15:12:38 +0100

    Remove de from config

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,5 @@
 const config = {
   port: Number(process.env.PORT) || 3001,
-  db: {
-    client: process.env.DB_CLIENT,
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      database: process.env.DB_DATABASE,
-      host: process.env.DB_HOST,
-      password: process.env.DB_PASSWORD,
-      port: process.env.DB_PORT,
-      user: process.env.DB_USER,
-    },
-  },
 }
 
 export default config
